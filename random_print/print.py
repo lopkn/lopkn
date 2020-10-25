1d58ac5ead98f75c57ca8034b69a6cdf139db4f3
lopkn <leoyuan06@gmail.com>
2020-10-25 12:37:11 +0800

    added grass

--- random_print/print.py
+++ random_print/print.py
@@ -1,12 +1,13 @@
 
+import math
 from random import random
 import sys
 class bcolors:
     HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    OKBLUE = '\033[1;94m'
+    OKCYAN = '\033[1;96m'
+    OKGREEN = '\033[1;38;5;34m'
+    OKYELLOW = '\033[1;4;93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
@@ -16,19 +17,37 @@ class bcolors:
 
 number = 1000
 for i in range(5000):
-    import math
     rng = int(math.ceil(random() * number))
     if rng > 900:
-        sys.stdout.write(bcolors.BOLDGREEN + "T" + bcolors.ENDC),
+        sys.stdout.write(bcolors.OKGREEN + "," + bcolors.ENDC),
         continue
-    if rng >= 800 and rng < 900:
-        sys.stdout.write(bcolors.OKCYAN + " 2 " + bcolors.ENDC),
+    if rng >= 850 and rng < 900:
+        sys.stdout.write(bcolors.OKCYAN + "=" + bcolors.ENDC),
         continue   
-    if rng >= 600 and rng < 800:
-        sys.stdout.write(bcolors.OKBLUE + "vvv" + bcolors.ENDC),
+    if rng >= 750 and rng < 800:
+        sys.stdout.write(bcolors.OKBLUE + "v" + bcolors.ENDC),
         continue   
     if rng == 55 :
-        sys.stdout.write(bcolors.UNDERLINE + ".//\." + bcolors.ENDC),
-        continue
+        sys.stdout.write(bcolors.UNDERLINE + " /\ " + bcolors.ENDC),
+        continue   
+    if rng == 56 :
+        sys.stdout.write(bcolors.UNDERLINE + "[^]" + bcolors.ENDC),
+        continue           
+    sys.stdout.write(bcolors.OKYELLOW + "--" + bcolors.ENDC)
+#for i in range(5000):
+    #rng = int(math.ceil(random() * number))
+    #if rng > 900:
+        #sys.stdout.write(bcolors.BOLDGREEN + "T" + bcolors.ENDC),
+        #continue
+    #if rng >= 800 and rng < 900:
+        #sys.stdout.write(bcolors.OKCYAN + "=" + bcolors.ENDC),
+        #continue   
+    #if rng >= 600 and rng < 800:
+        #sys.stdout.write(bcolors.OKBLUE + "vVv" + bcolors.ENDC),
+        #continue   
+    #if rng == 55 :
+        #sys.stdout.write(bcolors.UNDERLINE + ".//\." + bcolors.ENDC),
+        #continue
     #sys.stdout.write("0")         
-    sys.stdout.write(bcolors.OKGREEN + "0" + bcolors.ENDC)
+    #sys.stdout.write(bcolors.OKGREEN + ",," + bcolors.ENDC)
+
